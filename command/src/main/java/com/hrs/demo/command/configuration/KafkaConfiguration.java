1138801dbe7413d813e4448ee15fbb4f990f115e
hdcong <hdcong@email.com>
2024-06-13 20:27:04 +0700

    Fix missing .mvn

--- command/src/main/java/com/hrs/demo/command/configuration/KafkaConfiguration.java
+++ command/src/main/java/com/hrs/demo/command/configuration/KafkaConfiguration.java
@@ -1,13 +1,16 @@
 package com.hrs.demo.command.configuration;
 
+import com.hrs.demo.command.utils.Constants;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -32,4 +35,21 @@ public class KafkaConfiguration {
   public KafkaTemplate<String, Object> kafkaTemplate() {
     return new KafkaTemplate<>(producerFactory());
   }
+
+  @Bean
+  public NewTopic bookingTopic() {
+    return TopicBuilder.name(Constants.BOOKING_TOPIC)
+        .partitions(1)
+        .replicas(1)
+        .build();
+  }
+
+  @Bean
+  public NewTopic hotelTopic() {
+    return TopicBuilder.name(Constants.HOTEL_TOPIC)
+        .partitions(1)
+        .replicas(1)
+        .build();
+  }
+
 }
